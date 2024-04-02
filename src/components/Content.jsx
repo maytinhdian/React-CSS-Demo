eb7cae7b14e1e625dc45994c878518f9a0dbc9ba
Minh Triet <minhtriet@maytinhdian.com>
2024-04-03 06:40:57 +0700

    Project Initialized

---
+++ src/components/Content.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "../components/Content.module.css";
+
+function Content(props) {
+  return (
+    <div className={styles.content}>
+      <h1>This is main content off website</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis cum
+        non commodi laboriosam hic incidunt maxime accusantium ipsa id magnam
+        quasi dicta corrupti nam recusandae, sunt impedit eos ipsam rerum.
+      </p>
+      
+    </div>
+    
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../components/Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>Footer Demo Co</h1>
+        </div>
+    );
+}
+
+export default Footer;
